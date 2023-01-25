7b7ccb2b03f1b11c4bf7be2d25ad043e81a59520
Barry Lagerweij <b.lagerweij@carepay.com>
2023-01-25 15:38:10 +0100

    added brewfile and IntelliJ / VSCode plugins

---
+++ .bin/install-code.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+code --install-extension eamodio.gitlens
+code --install-extension GitHub.copilot
---
+++ .bin/install-idea.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins aws.toolkit
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins CheckStyle-IDEA
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins com.github.copilot
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins org.jetbrains.plugins.go
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins org.mapstruct.intellij
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins "PlantUML integration"
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins org.sonarlint.idea
+/Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPluginsorg.intellij.plugins.hcl
