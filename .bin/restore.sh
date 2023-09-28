f4e00e774432a6ce32bf46ab63c74e3c147971c9
Barry Lagerweij <blagerweij@gmail.com>
2023-09-28 20:53:42 +0200

    updates

--- .bin/restore.sh
+++ .bin/restore.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+brew bundle install
+
+npm install -g $(jq -r '.dependencies | keys | join("\n")' npm-global-package.json | sed '/npm/d;/corepack/d')
+
 for plugin in $(cat idea-plugins.txt); do
   /Applications/IntelliJ\ IDEA.app/Contents/MacOS/idea installPlugins $plugin
 done
