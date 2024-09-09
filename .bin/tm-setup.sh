959380089fff40a22cf013cb11cd376fe09b087a
Barry Lagerweij <b.lagerweij@carepay.com>
2024-09-09 17:28:57 +0200

    exclude time machine

---
+++ .bin/tm-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+tmutil addexclusion ~blagerweij/Downloads
+tmutil addexclusion ~blagerweij/go
+tmutil addexclusion ~blagerweij/Library/Containers
+tmutil addexclusion ~blagerweij/Library/Application\ Support
+tmutil addexclusion ~blagerweij/.cache
+tmutil addexclusion ~blagerweij/.docker
+tmutil addexclusion ~blagerweij/.gradle
+tmutil addexclusion ~blagerweij/.m2
+tmutil addexclusion ~blagerweij/.npm
+tmutil addexclusion ~blagerweij/.nvm
+tmutil addexclusion ~blagerweij/.sonar
+tmutil addexclusion ~blagerweij/.vscode
