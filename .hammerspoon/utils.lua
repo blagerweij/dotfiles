55384891f16d3400d97498ebc9c8972078c3b0a3
Barry Lagerweij <b.lagerweij@carepay.com>
2024-09-17 00:24:49 +0200

    updated zoom support, scan for outlook meetings

--- .hammerspoon/utils.lua
+++ .hammerspoon/utils.lua
@@ -40,4 +40,48 @@ function This:reverse(table)
     end
 end
 
+local function urlencode(s) return s and (s:gsub("%W", function (c) return string.format("%%%02x", c:byte()); end)); end
+local function urldecode(s) return s and (s:gsub("%%(%x%x)", function (c) return string.char(tonumber(c,16)); end)); end
+
+local function _formencodepart(s)
+    return s and (s:gsub("%W", function (c)
+        if c ~= " " then
+            return string.format("%%%02x", c:byte())
+        else
+            return "+"
+        end
+    end))
+end
+
+function This:formencode(form)
+    local result = {}
+    for name, value in pairs(form) do
+        table.insert(result, _formencodepart(name).."=".._formencodepart(value))
+    end
+    return table.concat(result, "&")
+end
+
+function This:formdecode(s)
+    local r = {}
+    for k, v in s:gmatch("([^=&]*)=([^&]*)") do
+        k, v = k:gsub("%+", "%%20"), v:gsub("%+", "%%20")
+        k, v = urldecode(k), urldecode(v)
+        r[k] = v
+    end
+    return r
+end
+
+local _CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+math.randomseed(hs.timer.absoluteTime())
+
+function This:randomString(length)
+    local ret = {}
+    local r
+    for i = 1, length do
+        r = math.random(1, #_CHARSET)
+        table.insert(ret, _CHARSET:sub(r, r))
+    end
+    return table.concat(ret)
+end
+
 return This
